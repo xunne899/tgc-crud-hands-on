2957fc43636c73604c9b00246f646643a41b7e0c
xunne899 <tan.yixun@trentglobal.edu.sg>
2022-06-06 08:55:27 +0000

    add new books content

--- index.js
+++ index.js
@@ -8,11 +8,35 @@ const app = express();
 app.set('view engine', 'hbs');
 
 wax.on(hbs.handlebars);
-wax.setLayoutPath('.views/layouts');
+wax.setLayoutPath('views/layouts');
+
+
+
+app.use(express.urlencoded({
+    'extended':false
+}))
 
 app.get('/', function(req,res){
-    res.send("It's alive!")
+    let books = database.getAll()
+    res.render('books.hbs', {
+        'books': books
+      })
 })
+
+// create
+app.get('/add', async(req,res)=>{
+    res.render('add-books.hbs');
+  })
+  
+  // post 
+  app.post('/add', async(req,res)=>{
+  
+      let title = req.body.title;
+      let isbn =  req.body.isbn
+   database.addBook(title, isbn)
+   res.redirect('/')
+   
+  })
 
 app.listen(3000, function(){
     console.log("Server has started");
